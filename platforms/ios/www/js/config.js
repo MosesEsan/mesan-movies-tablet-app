3964ff00a2aab75e6116307d52863ae4e2194354
Moses Esan <mosesesan@hotmail.com>
2015-09-25 19:33:29 +0100

    Revert "Updated Movies Page Design and Layout"
    This reverts commit 69e90724113136e18c07033d7e1aa238916ea316.

--- platforms/ios/www/js/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by mosesesan on 24/09/15.
- */
-var config_module = angular.module('myApp.config', [])
-        .constant('APP_NAME','MEP')
-        .constant('APP_VERSION','0.1')
-        .constant('API_KEY','a3e3a1d79235a1ef189dbb0c243f240e')
-        .constant('IMG_URL','http://image.tmdb.org/t/p/w500')
-    ;
